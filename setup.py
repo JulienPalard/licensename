e6893a2005da1084edf10babc1cef8b2cd073e9d
Julien Palard <julien@palard.fr>
2017-04-25 22:22:14 +0200

    Drop setup.cfg, was probably only working with magic setup of pyscaffold.

--- setup.py
+++ setup.py
@@ -12,12 +12,42 @@ import sys
 from setuptools import setup, find_packages
 
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=sphinx,
-          package_dir={'': 'src'},
-          packages=find_packages('src'))
+    setup(
+        name='licensename',
+        version='0.4.1',
+        description='Find a license name from a license file.',
+        long_description=readme,
+        author='Julien Palard',
+        author_email='julien@palard.fr',
+        license="MIT",
+        url='https://github.com/JulienPalard/licensename',
+        entry_points={
+            'console_scripts': [
+                 'licensename=licensename.licensename:run'
+            ]
+        },
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+        ],
+        setup_requires=sphinx,
+        package_dir={'': 'src'},
+        install_requires=['textunwrap'],
+        packages=find_packages('src'))
 
 
 if __name__ == "__main__":
